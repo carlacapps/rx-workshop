39e7a63ce11b863908d8470bda87f1a98783c164
jlowery <jlowery@paypal.com>
2019-03-01 02:34:35 -0600

    Add challenge question to array step

--- 01-basic-array.js
+++ 01-basic-array.js
@@ -3,14 +3,37 @@ var names = [ 'Sarah Smith', 'Adam Scott', 'Eve Livingston'];
 
 // Pipeline
 var usersPipeline = names.map(function (name) {
-		/*
+		/**
 		 * return { firstName: result[0], lastName: result[1] }
 		*/
 	})
 	.map(function (user) {
-		/*
+		/**
 		 * return { firstName: "John", lastName: "Doe", userName: "johnDoe" }
 		 */
 	});
+
+	/**
+	 * Why TWO maps? Good question!
+	 */
+
+/**
+ * Want more of a challenge? Use a `reduce()` and create a user object, rather than a collection
+ * keyed off of the `userName` you just created. E.g.:
+ *
+ * {
+ *    sarahSmith: {
+ *      firstName: "Sarah",
+ *      lastName: "Smith",
+ *      userName: "sarahSmith"
+ *    },
+ *    adamScott: {
+ *      firstName: "Adam",
+ *      lastName: "Scott",
+ *      userName: "adamScott"
+ *    }
+ *    // ...
+ * }
+ */
 
 console.log(usersPipeline);
